4bb4d9abc8eec20a8f9072e9d3e41d90682e82bb
Archit Gaur <44581304+ArchitG358@users.noreply.github.com>
2021-03-31 22:33:41 +0530

    Update urls-validator.py

--- urls-validator.py
+++ urls-validator.py
@@ -4,11 +4,12 @@ import requests
 import os
 from pathlib import Path
 
-folder = Path(r'/home/archit/Desktop/recon/urls/').rglob('*.txt')
+folder = Path(r'/home/archit/Desktop/fetlife/fetlife.com/js/').rglob('*.txt')
 
 files = [x for x in folder]
 
 working_urls = []
+manual_urls = []
 
 for name in files:
 	f = open(name, 'r')
@@ -16,21 +17,22 @@ for name in files:
 	print(name)	
 	#print(content)	
 	for url in content:
-		try:
-			res = requests.get(url)
+		try:	
+			res = requests.get(url.split()[0])			
 			if res.status_code < 400:
-				# print(url, "yes")
+			#	print(url, "yes")
 				working_urls.append(url)
 			#else:
 			#	print(url, "no")
-		except (requests.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.InvalidSchema, requests.exceptions.InvalidURL):
-			print("could not get")
-	
+		except (requests.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.InvalidSchema, requests.exceptions.InvalidURL) :
+			# print("could not get")
+			manual_urls.append(url)
 	f.close()
-
 
 output_file = open("valid_urls_checked.txt", "w")
 output_file.writelines(working_urls)
 output_file.close()
 
-
+output_file = open("manual_verify_urls.txt", "w")
+output_file.writelines(manual_urls)
+output_file.close()
